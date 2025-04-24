49df6f5a1f527a748b6ca973ec7352db70881b64
ESHAB SACHAN <156100995+Eshab01@users.noreply.github.com>
2025-04-24 23:35:03 +0530

    Add files via upload

---
+++ Portfolio new/script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const observerOptions = {
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+
+    document.querySelectorAll('.animate').forEach(section => {
+        observer.observe(section);
+    });
+});
